d8d944fb579c8232b27dda6d8b120ab2712c2ef2
Lucas Alves <lucasalvessm@outlook.com>
2022-06-17 14:38:25 +0100

    Add auth interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -11,11 +11,18 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './components/home/home.component';
 import { ConfirmModalComponent } from './components/confirm-modal/confirm-modal.component';
+import { AuthInterceptor } from './authorization/interceptors/auth.interceptor';
 
 @NgModule({
   declarations: [AppComponent, PageNotFoundComponent, MenuComponent, HomeComponent, ConfirmModalComponent],
   imports: [BrowserModule, AppRoutingModule, NgbModule, HttpClientModule, SharedModule, BrowserAnimationsModule],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
